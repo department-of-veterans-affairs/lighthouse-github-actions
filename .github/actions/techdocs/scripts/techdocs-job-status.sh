c96f26a3dc4a8a4bdcd52d13f7e156c37d7d6784
Nick Luckey <nickluckey@gmail.com>
2022-02-23 14:19:26 +0000

    Added clean up

--- .github/actions/techdocs/scripts/techdocs-job-status.sh
+++ .github/actions/techdocs/scripts/techdocs-job-status.sh
@@ -17,7 +17,7 @@ check_required_environment() {
 
 await_job() {
   repo=$1
-  job_name="lighthouse-techdocs-${repo}"
+  local job_name="lighthouse-techdocs-${repo}"
   while ! bash -c "kubectl get job.batch/${job_name} | grep \"1/1\"" > /dev/null 2>&1; do 
     kubectl logs -f -l app=${job_name}
     sleep 15;
@@ -25,22 +25,33 @@ await_job() {
         echo "Techdocs status: Error";
         kubectl get pods 
         kubectl logs -f -l app="${job_name}"
-        exit 1;
+        clean_up
     fi
     if kubectl get pods | grep "${job_name}" | grep "0/1" | grep "CrashLoopBackOff"; then
         echo "Techdocs status: CrashLoopBackOff";
         kubectl logs -f -l app="${job_name}"
-        exit 1;
+        clean_up
     fi
   done
 
+  echo "Techdocs Job complete"
+}
+
+clean_up() {
+  repo_name=${1}
+  local job_name="lighthouse-techdocs-${repo}"
+  echo "Cleaning up resources..."
+  kubectl delete secret lighthouse-techdocs-${repo_name}-secrets
+  kubectl delete job.batch/${job_name}
+  echo "Clean up complete"
+  exit 0
 }
 
 run_main() {
   repo=${1##*/}
 
   await_job "${repo}" || exit 1
-  echo "Techdocs Job complete"
+  clean_up "${repo}" || exit 1
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
