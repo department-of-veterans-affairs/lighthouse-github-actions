3c85bd391d654847ce47cd29f427594c061d6e4a
Nick Luckey <nickluckey@gmail.com>
2022-02-23 01:26:58 +0000

    Updated logging

--- .github/actions/techdocs/scripts/techdocs-job-status.sh
+++ .github/actions/techdocs/scripts/techdocs-job-status.sh
@@ -4,22 +4,19 @@ set -euo pipefail
 
 
 check_required_environment() {
-    local required_env=""
+  local required_env=""
 
-    for reqvar in $required_env; do
-        if [ -z "${!reqvar:0:10}" ]; then
-        raise "missing ENVIRONMENT VARIABLE ${reqvar}"
-        return 1
-        fi
-    done
+  for reqvar in $required_env; do
+      if [ -z "${!reqvar:0:10}" ]; then
+      raise "missing ENVIRONMENT VARIABLE ${reqvar}"
+      return 1
+      fi
+  done
 }
 
-stream_output() {
-  kubectl logs -f -l app=lighthouse-techdocs
-}
 await_job() {
 
-while ! bash -c 'kubectl get job.batch/lighthouse-techdocs | grep "1/1"' > /dev/null 2>&1; do 
+  while ! bash -c 'kubectl get job.batch/lighthouse-techdocs | grep "1/1"' > /dev/null 2>&1; do 
     kubectl logs -f -l app=lighthouse-techdocs
     sleep 15;
     if kubectl get pods | grep "lighthouse-techdocs" | grep "0/1" | grep "Error"; then
@@ -33,13 +30,13 @@ while ! bash -c 'kubectl get job.batch/lighthouse-techdocs | grep "1/1"' > /dev/
         kubectl logs -f -l app=lighthouse-techdocs
         exit 1;
     fi
-done
+  done
 
 }
 
 run_main() {
-    await_job || exit 1
-    echo "Techdocs Job complete"
+  await_job || exit 1
+  echo "Techdocs Job complete"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
