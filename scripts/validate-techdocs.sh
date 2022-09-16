e6fa05373d711a6cc073ede4561dfb7cd0d438b1
Nick Luckey <nickluckey@gmail.com>
2022-09-16 17:49:45 +0000

    [API-19475] Updated validation script
    Signed-off-by: Nick Luckey <nickluckey@gmail.com>

--- scripts/validate-techdocs.sh
+++ scripts/validate-techdocs.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-testing_workflow() {
-  echo "I'm a real workflow!"
+setup_mkdocs() {
+  /bin/bash <(curl -s https://${TOKEN}@raw.githubusercontent.com/department-of-veterans-affairs/lighthouse-developer-portal/main/scripts/setup_mkdocs.sh)
+}
+
+build_with_mkdocs() {
+  build_mkdocs=$(mkdocs build --strict)
+  echo "${build_mkdocs}"
+}
+
+build_with_techdocs() {
+  build_techdocs=$(techdocs-cli generate --no-docker -v)
 }
 
 main() {
-  testing_workflow || exit 1
+  local results
+  setup_mkdocs || exit 1
+  results+=$(build_with_mkdocs)
+  results+=$(build_with_techdocs)
+  echo "::set-output name=results::${results}"
 }
 
 main
