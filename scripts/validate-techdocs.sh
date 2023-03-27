7f0b0e151fc18e4bed1213d19c0f35af0c331bfe
declan-downie <109244226+declan-downie@users.noreply.github.com>
2023-03-27 15:36:37 +0000

    [API-24511] Test upgrading mkdocs to 1.4.2

--- scripts/validate-techdocs.sh
+++ scripts/validate-techdocs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 setup_mkdocs() {
-  /bin/bash <(curl -s https://${TOKEN}@raw.githubusercontent.com/department-of-veterans-affairs/lighthouse-github-actions/main/scripts/setup_mkdocs.sh)
+  /bin/bash <(curl -s https://${TOKEN}@raw.githubusercontent.com/department-of-veterans-affairs/lighthouse-github-actions/API-24511-disable-production-publish/scripts/setup_mkdocs.sh)
 }
 
 build_with_mkdocs() {
