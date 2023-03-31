405354bbda37ebda7827c81f5fb2bb688b788052
Declan Downie <109244226+declan-downie@users.noreply.github.com>
2023-03-31 15:37:34 -0400

    [API-25012] Add testing parameters (#102)
    * [API-25012] Retag techdocs image

--- scripts/create-techdocs-job.sh
+++ scripts/create-techdocs-job.sh
@@ -70,7 +70,7 @@ spec:
             memory: 450Mi
       containers:
       - name: techdocs
-        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/techdocs:API-25012
+        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/techdocs:fb868c567d79ab5d2118121ca1a3e4467bfedb40
         imagePullPolicy: Always
         command: ['/bin/sh']
         args:
