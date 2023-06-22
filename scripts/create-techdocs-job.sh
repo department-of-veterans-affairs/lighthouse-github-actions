4dc9a799daa5dad5c5af5666e51c0cad1a3999e2
Declan Downie <109244226+declan-downie@users.noreply.github.com>
2023-06-22 20:47:32 +0000

    [API-25869] Re-enable and test production publish

--- scripts/create-techdocs-job.sh
+++ scripts/create-techdocs-job.sh
@@ -58,7 +58,7 @@ spec:
       serviceAccountName: ${service_account_name}
       initContainers:
       - name: git-sync
-        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/git-sync:edge
+        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/git-sync:23.06.1
         command: ['/git-sync']
         args: ${git_sync_args}
         volumeMounts:
@@ -70,7 +70,7 @@ spec:
             memory: 450Mi
       containers:
       - name: techdocs
-        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/techdocs:edge
+        image: ghcr.io/department-of-veterans-affairs/lighthouse-developer-portal/techdocs:23.06.1
         imagePullPolicy: Always
         command: ['/bin/sh']
         args:
