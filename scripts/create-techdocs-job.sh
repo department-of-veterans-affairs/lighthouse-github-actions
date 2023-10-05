522e0989868b4b8bbd9a03de7d27ae0a8b8daaca
Declan Downie <109244226+declan-downie@users.noreply.github.com>
2023-10-05 18:02:33 +0000

    [API-30210] Revert to setting docker-creds in workflow

--- scripts/create-techdocs-job.sh
+++ scripts/create-techdocs-job.sh
@@ -92,6 +92,8 @@ spec:
           limits:
             cpu: 500m
             memory: 1024Mi
+      imagePullSecrets:
+        - name: "${ghcr_secrets}"
       restartPolicy: Never
       volumes:
       - name: repo
